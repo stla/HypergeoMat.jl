e4d491000e288ffd2705ee2eb19ddd7cebdf0b8b
Documenter.jl <documenter@juliadocs.github.io>
2020-12-12 09:48:38 +0000

    build based on 3865bf7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#HypergeoMat.jl-Documentation","page":"Documentation","title":"HypergeoMat.jl Documentation","text":"","category":"section"},{"location":"","page":"Documentation","title":"Documentation","text":"Modules = [HypergeoMat]\nOrder   = [:type, :function]","category":"page"}]
+}
