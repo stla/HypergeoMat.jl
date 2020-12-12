a7a34d47cfb715a88733b04d61286d1786243ed7
stla <laurent_step@yahoo.fr>
2020-12-12 20:20:16 +0100

    first deploy doc

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#HypergeoMat.jl-Documentation","page":"Documentation","title":"HypergeoMat.jl Documentation","text":"","category":"section"},{"location":"index.html","page":"Documentation","title":"Documentation","text":"Modules = [HypergeoMat]\nOrder   = [:type, :function]","category":"page"}]
+}
