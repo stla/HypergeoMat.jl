34953efbd1c8afbbffa7753945fc64c90da96dff
stla <laurent_step@yahoo.fr>
2020-12-12 15:54:26 +0100

    test 0F0 and 1F0

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,37 @@
 using HypergeoMat
 using Test
+using LinearAlgebra
+
+@testset "Hypergeo0F0 is exponential of trace" begin
+  X1 = [0.3 0.2 0.1; 0.2 0.3 0.2; 0.1 0.2 0.3]
+  X2 = [0.3im 0.2 0.1; 0.2 0.3im 0.2; 0.1 0.2 0.3im]
+  @test isapprox(
+    hypergeomPQ(10, Float64[], Float64[], X1),
+    exp(tr(X1))
+  )
+  @test isapprox(
+    hypergeomPQ(10, Float64[], Float64[], X2),
+    exp(tr(X2))
+  )
+end
+
+@testset "Hypergeo1F0 is det(I-X)^(-a)" begin
+  X1 = [0.03 0.02 0.01; 0.02 0.03 0.02; 0.01 0.02 0.03]
+  X2 = [0.02 0.02 0.01; 0.02 0.02 0.02; 0.01 0.02 0.02]
+  X3 = [0.03im 0.02 0.01; 0.02 0.03im 0.02; 0.01 0.02 0.03im]
+  @test isapprox(
+    hypergeomPQ(15, [3.0], Float64[], X1),
+    det(I - X1)^(-3)
+  )
+  @test isapprox(
+    hypergeomPQ(15, [4.0im], Float64[], X2),
+    det(I - X2)^(-4.0im)
+  )
+  @test isapprox(
+    hypergeomPQ(15, [3.0], Float64[], X3),
+    det(I - X3)^(-3)
+  )
+end
 
 @testset "Hypergeo2F1" begin
   @testset "some values" begin
